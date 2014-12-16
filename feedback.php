182f60038d8dfa9c8f6abdfd42da4e64e88f32e1
Venky Mullapudi <something.venky@gmail.com>
2014-12-16 15:35:35 +0530

    Create feedback.php

---
+++ feedback.php
@@ -0,0 +1,30 @@
+<style>
+#body_wrap {
+	text-align: center;
+    padding-left: 20px;
+	padding-right: 20px;
+	padding-bottom: 20px;
+	padding-top: 80px;
+    -webkit-transition: all 0.5s ease;
+    -moz-transition: all 0.5s ease;
+    -o-transition: all 0.5s ease;
+    transition: all 0.5s ease;
+	color: #3e9cbf;
+}
+
+</style>
+<title>Travel Easy Indian: Feedback</title>
+<?php include('header.php'); ?>
+<div id="body_wrap">
+<b>
+We are thrilled to have you considered to give us some feedback
+</br>Much appreciated.
+</br></br>
+Your Full Name:<br><input type="text" name="realname" placeholder="Full Name"></br>
+Your Email ID:<br><input type="text" name="email" placeholder="Email ID"></br></br>
+Feedback:</br>
+<textarea name="feedback" rows="7" cols="30" placeholder="Feedback. Not more than 200 words">
+</textarea> 
+</b>
+</div>
+<?php include('footer.php'); ?>
