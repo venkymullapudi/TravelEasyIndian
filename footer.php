5a00b9a25c32a3b86cae5b8614006b22ea0fe3ea
Venky Mullapudi <something.venky@gmail.com>
2014-12-16 15:35:54 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,35 @@
+<style>
+#footer
+{
+	z-index: 1000;
+    clear: both;
+    border: 1px groove #EBE1DB;
+    background: #EBE1DB;
+    color: #3e9cbf;
+    padding: 0;
+    text-align: center;
+    vertical-align: middle;
+    line-height: normal;
+    margin: 0;
+    position: fixed;
+    bottom: 0px;
+    width: 110%;
+	left: -5%;
+}
+a{
+text-align: center;
+font-size: 20px;
+font-family: Helvetica Neue, Helvetica, Arial;
+color: #3e9cbf;
+text-decoration: none;
+}
+</style>
+
+<div class="footer" id="footer">
+<center>
+<a href="about.php"  style="font-family: 'Cookie', Helvetica Neue, Helvetica, Arial;font-size: 28px;">About</a> &nbsp;&nbsp;
+<a href="suggestion.php" style="font-family: 'Cookie', Helvetica Neue, Helvetica, Arial;font-size: 28px;">Suggestion</a>
+</center>
+</div>
+
+
