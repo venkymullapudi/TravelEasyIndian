b3f0b8d3e047771fa5ee39fcdceee7fad884cefd
Venky Mullapudi <something.venky@gmail.com>
2014-12-16 15:38:10 +0530

    Create suggestion.php

---
+++ suggestion.php
@@ -0,0 +1,31 @@
+<style>
+#body_wrap {
+	text-align: center;
+    padding-left: 20px;
+	padding-right: 20px;
+	padding-bottom: 20px;
+	padding-top: 80px;
+    -webkit-transition: all 0.5s ease;
+    -moz-transition: all 0.5s ease;
+    -o-transition: all 0.5s ease;
+    transition: all 0.5s ease;
+	color: #3e9cbf;
+}
+
+</style>
+<title>Travel Easy Indian: Suggestion</title>
+<?php include('header.php'); ?>
+<div id="body_wrap">
+<b>
+Give us some feedback</b> or </br>
+<b>Suggest a travelling source and destination and the time of your convenience,
+</br>We will submit the requests to the concerned Travel authorities on your behalf.</b>
+</br></br>
+Your Full Name:<br><input type="text" name="realname" placeholder="Full Name"></br>
+Your Email ID:<br><input type="text" name="email" placeholder="Email ID"></br></br>
+Suggestion:</br>
+<textarea name="suggestatravel" rows="7" cols="30" placeholder="Travel suggestion. Not more than 100 words">
+</textarea> 
+</b>
+</div>
+<?php include('footer.php'); ?>
