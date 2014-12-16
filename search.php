94544f7d34854fcfcda2edb6b2951822a6c172cf
Venky Mullapudi <something.venky@gmail.com>
2014-12-16 15:37:48 +0530

    Create search.php

---
+++ search.php
@@ -0,0 +1,14 @@
+<?php
+$from=$_GET['from'];
+$to=$_GET['to'];
+$date=$_GET['date'];
+//$time=$_GET['time'];
+?>
+
+<html>
+<body>
+<?php include('header.php'); ?>
+<?php include('result.php'); ?>
+<?php include('footer.php'); ?>
+</body>
+</html>
