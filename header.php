e80d397355081c7cf573bb979552d32bdb7d4f5f
Venky Mullapudi <something.venky@gmail.com>
2014-12-16 15:36:11 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,39 @@
+<style>
+#header {
+	 z-index: 1000;
+     position:fixed;
+     margin:0px;
+     top:0px;
+     left:0px;
+     display:block;
+     width:100%;
+     height:40px;
+     -moz-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.5);
+     background:#EBE1DB;
+     background: -moz-linear-gradient(center top , #EBE1DB, #EBE1DB) repeat scroll 0 0 transparent;
+	 margin-bottom: 10px;
+}
+#header_nav {
+text-align: center;
+margin-top: 7px;
+}
+a{
+text-align: center;
+font-size: 20px;
+font-family: 'Cookie', Helvetica Neue, Helvetica, Arial;
+color: #3e9cbf;
+text-decoration: none;
+}
+
+</style>
+<link id="page_favicon" href="images/favicon.ico" rel="icon" type="image/x-icon" />
+<title>Travel Easy Indian</title>
+<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+<div id="header">
+   <ul id="header_nav">
+    <form action="search.php" method="get">
+      <a href="http://localhost/traveleasyindian/" style="font-family: 'Cookie', Helvetica Neue, Helvetica, Arial;font-size: 28px;">Travel Easy Indian</a>
+
+	 </form>
+   </ul>
+</div>
