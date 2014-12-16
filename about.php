cb2a03f0e24da79a13dbb317c4516b572b90ffc6
Venky Mullapudi <something.venky@gmail.com>
2014-12-16 15:35:16 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<style>
+#body_wrap {
+	text-align: center;
+    padding-left: 20px;
+	padding-right: 20px;
+	padding-bottom: 20px;
+	padding-top: 80px;
+    -webkit-transition: all 0.5s ease;
+    -moz-transition: all 0.5s ease;
+    -o-transition: all 0.5s ease;
+    transition: all 0.5s ease;
+	color: #3e9cbf;
+}
+
+</style>
+
+<?php include('header.php'); ?>
+<div id="body_wrap">
+<h1>
+A simple clutter free application,
+to help Indian travelers compare their travel options.
+</h1>
+</div>
+<?php include('footer.php'); ?>
