2a5297ebf29a09782056d320c78e5e83bb58b308
Venky Mullapudi <something.venky@gmail.com>
2015-04-01 18:21:11 +0530

    Update include.php

--- include.php
+++ include.php
@@ -2,8 +2,8 @@
 $DBSERVERNAME = "localhost";
 $DBUSERNAME = "root";
 $DBUSERPASSWORD = "";
-$DBNAME = "baas";
+$DBNAME = "tea";
 $THEME = "DefaultTheme";
-$conn = mysql_connect($DBSERVERNAME, $DBUSERNAME, $DBUSERPASSWORD) or die  (mysql_error());
-$db=mysql_select_db($DBNAME);
+//$conn = mysql_connect($DBSERVERNAME, $DBUSERNAME, $DBUSERPASSWORD) or die  (mysql_error());
+//$db=mysql_select_db($DBNAME);
 ?>
